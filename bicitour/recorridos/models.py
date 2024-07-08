d90ccefd1112af433a389b1bc7361e98b57537d7
rodrigo <mediamedina92@gmail.com>
2024-07-08 00:22:15 -0600

    Creacion del panel de administrador y superusuarios

--- bicitour/recorridos/models.py
+++ bicitour/recorridos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Recorridos(models.Model): #Define la estructura de nuestra tabla
+    fecha = models.DateTimeField(auto_now_add=True) #Texto largo
+    hora = models.TimeField(auto_now_add=True)
+    estado = models.TextField(max_length=15)
+    ciudad = models.TextField() #Fecha y tiempo
+    km_recorrido = models.FloatField()
+    tiempo_estimado = models.TimeField()
+    punto_inicio = models.TextField()
+    costo = models.IntegerField()
+    foto_zona_visitar = models.TextField()
+    activo = models.BooleanField(True)
